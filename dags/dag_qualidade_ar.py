671b02b8a2c24c43d0c15f6a1c690978bb111112
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2024-11-14 13:14:07 -0300

    Configuração do Airflow para salvar dados das APIs Mapbiomas e AQICN

--- dags/dag_qualidade_ar.py
+++ dags/dag_qualidade_ar.py
@@ -1,43 +1,57 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.python_operator import BranchPythonOperator
-from airflow.operators.bash import BashOperator
 import os
 
-from tasks.raw_extract_data_api import raw_extract_data_api
+from tasks.raw_extract_data_api import raw_extract_mapbiomas_api, raw_extract_aqicn_api
 from tasks.stg_transform_data import stg_transform_data
 from tasks.trusted_refine_data import trusted_refine_data
 from tasks.application_view_data import application_view_data
 
+# Instalação de pacotes necessários (somente necessário se não estiverem instalados no ambiente)
 os.system('pip install fastparquet')
 os.system('pip install requests')
 
 with DAG(
     'dag_qualidade_ar',
-    start_date = datetime(2024,10,20),
-    schedule_interval = '0 8 * * *',
+    start_date=datetime(2024, 10, 20),
+    schedule_interval='0 8 * * *',
     catchup=False,
     max_active_runs=1,
     dagrun_timeout=timedelta(minutes=20)
-    ) as dag:
+) as dag:
 
-  extract_data_api = PythonOperator(
-      task_id = 'extract_data_api',
-      python_callable = raw_extract_data_api,
-      execution_timeout=timedelta(minutes=5)
-  )
-  transform_data = PythonOperator(
-      task_id = 'transform_data',
-      python_callable = stg_transform_data
-  )
-  refine_data = PythonOperator(
-      task_id = 'refine_data',
-      python_callable = trusted_refine_data
-  )
-  view_data = PythonOperator(
-      task_id = 'view_data',
-      python_callable = application_view_data
-  )
+    # Operador para extrair dados da API Mapbiomas
+    extract_mapbiomas_api = PythonOperator(
+        task_id='extract_mapbiomas_api',
+        python_callable=raw_extract_mapbiomas_api,
+        execution_timeout=timedelta(minutes=5)
+    )
 
-extract_data_api >> transform_data >> refine_data >> view_data
+    # Operador para extrair dados da API AQICN
+    extract_aqicn_api = PythonOperator(
+        task_id='extract_aqicn_api',
+        python_callable=lambda: raw_extract_aqicn_api("vitoria"),  # Modifique a cidade conforme necessário
+        execution_timeout=timedelta(minutes=5)
+    )
+
+    # Operador para transformar dados
+    transform_data = PythonOperator(
+        task_id='transform_data',
+        python_callable=stg_transform_data
+    )
+
+    # Operador para refinar dados
+    refine_data = PythonOperator(
+        task_id='refine_data',
+        python_callable=trusted_refine_data
+    )
+
+    # Operador para visualizar dados
+    view_data = PythonOperator(
+        task_id='view_data',
+        python_callable=application_view_data
+    )
+
+    # Definindo as dependências das tarefas
+    [extract_mapbiomas_api, extract_aqicn_api] >> transform_data >> refine_data >> view_data
--- dags/tasks/raw_extract_data_api.py
+++ dags/tasks/raw_extract_data_api.py
@@ -1,11 +1,11 @@
 import requests
 import json
 import pandas as pd
-from airflow.hooks.base_hook import BaseHook
+from airflow.hooks.base import BaseHook
 from minio import Minio
 from io import BytesIO
 
-# Obter o token do mapbiomas
+# Função para obter o token do Mapbiomas
 def obter_token():
     connection = BaseHook.get_connection('mapbiomas_login')
     url = connection.host
@@ -35,25 +35,28 @@ def obter_token():
     else:
         raise Exception(f"Erro na autenticação: {auth_response.status_code} - {auth_response.text}")
 
-try:
-    token = obter_token()
-    print(f"Token obtido: {token}")
-except Exception as e:
-    print(e)
+# Função para salvar dados como JSON no MinIO
+def salvar_dados_minio(client, bucket_name, file_name, data):
+    json_object = json.dumps(data, indent=2).encode("utf-8")
+    client.put_object(
+        bucket_name,
+        file_name,
+        data=BytesIO(json_object),
+        length=len(json_object),
+        content_type="application/json"
+    )
+    print(f"Arquivo '{file_name}' salvo no bucket '{bucket_name}' do MinIO com sucesso.")
 
-# Extrair json da API
-def raw_extract_data_api():
-    # Configurações do MinIO no Docker
+# Função para extrair e processar dados da API Mapbiomas e salvar DataFrames no MinIO
+def raw_extract_mapbiomas_api():
     minio_connection = BaseHook.get_connection('minio')
     host = minio_connection.host + ':' + str(minio_connection.port)
     client = Minio(host, secure=False, access_key=minio_connection.login, secret_key=minio_connection.password)
     BUCKET = "raw"
-    buckets = client.list_buckets()
-    buckets = [i.name for i in buckets]
-    if BUCKET not in buckets:
+    if not client.bucket_exists(BUCKET):
         client.make_bucket(BUCKET)
     
-    # Acessar API
+    # Obter dados da API Mapbiomas
     token = obter_token()
     url = "https://plataforma.alerta.mapbiomas.org/api/v2/graphql"
     headers_with_token = {
@@ -97,36 +100,84 @@ def raw_extract_data_api():
       }
     }
     """
-    payload_data = {
-        "query": query_data
-    }
-    response_data = requests.post(url, json=payload_data, headers=headers_with_token)
+    response_data = requests.post(url, json={"query": query_data}, headers=headers_with_token)
+    
     if response_data.status_code == 200:
         data = response_data.json()
         if 'errors' in data:
-            print("Erro na resposta da API:")
+            print("Erro na resposta da API Mapbiomas:")
             print(json.dumps(data['errors'], indent=2))
         else:
-            print("Dados retornados com sucesso pela API")
-            bucket_name = "raw"
-            file_name = "data_raw.json"
-            json_object = json.dumps(data, indent=2).encode("utf-8")
-            
-            # Salvar dados na API
-            client.put_object(
-              bucket_name,
-              file_name,
-              data=BytesIO(json_object),
-              length=len(json_object),
-              content_type="application/json"
-            )
+            print("Dados retornados com sucesso pela API Mapbiomas")
+            salvar_dados_minio(client, BUCKET, "mapbiomas_data_raw.json", data)
 
-            print(f"Arquivo '{file_name}' salvo no bucket '{bucket_name}' do MinIO com sucesso.")
+            # Processar dados e criar DataFrames
+            dados_alertas = data['data']['alerts']
+            df_alertas = pd.DataFrame(dados_alertas['collection'])
+            df_biomas = pd.DataFrame(dados_alertas['rankingByBiome'])
+            df_cidades = pd.DataFrame(dados_alertas['rankingByCity'])
+            df_estados = pd.DataFrame(dados_alertas['rankingByState'])
+            df_resumo = pd.DataFrame([dados_alertas['summary']])
+
+            # Salvar DataFrames processados como JSON no MinIO
+            salvar_dados_minio(client, BUCKET, "df_alertas.json", df_alertas.to_dict(orient="records"))
+            salvar_dados_minio(client, BUCKET, "df_biomas.json", df_biomas.to_dict(orient="records"))
+            salvar_dados_minio(client, BUCKET, "df_cidades.json", df_cidades.to_dict(orient="records"))
+            salvar_dados_minio(client, BUCKET, "df_estados.json", df_estados.to_dict(orient="records"))
+            salvar_dados_minio(client, BUCKET, "df_resumo.json", df_resumo.to_dict(orient="records"))
     else:
-        print(f"Erro ao obter os dados: {response_data.status_code}")
+        print(f"Erro ao obter os dados Mapbiomas: {response_data.status_code}")
         print(response_data.text)
 
+# Função para consultar a API AQICN e processar dados de qualidade do ar
+def raw_extract_aqicn_api(cidade):
+    minio_connection = BaseHook.get_connection('minio')
+    host = minio_connection.host + ':' + str(minio_connection.port)
+    client = Minio(host, secure=False, access_key=minio_connection.login, secret_key=minio_connection.password)
+    BUCKET = "raw"
+    if not client.bucket_exists(BUCKET):
+        client.make_bucket(BUCKET)
+
+    token_aqicn = "3d2600ab777bcf8d09fbb8f87dd6bcb27713544d"
+    # token_aqicn = BaseHook.get_connection('aqicn_api').password
+    url_aqicn = f"https://api.waqi.info/feed/{cidade}/?token={token_aqicn}"
+    response_aqicn = requests.get(url_aqicn)
+
+    if response_aqicn.status_code == 200:
+        data = response_aqicn.json()
+        if data['status'] == 'ok':
+            print(f"Dados de qualidade do ar para {cidade} obtidos com sucesso.")
+            salvar_dados_minio(client, BUCKET, f"{cidade}_aqicn_data_raw.json", data)
+            
+            # Processar previsões de qualidade do ar
+            forecast_data = data['data']['forecast']['daily']
+            forecast_dfs = []
+            for pollutant in ['o3', 'pm10', 'pm25', 'uvi']:
+                if pollutant in forecast_data:
+                    df = pd.DataFrame(forecast_data[pollutant])
+                    df['pollutant'] = pollutant.upper()
+                    forecast_dfs.append(df)
+            if forecast_dfs:
+                forecast_df = pd.concat(forecast_dfs, ignore_index=True)
+                salvar_dados_minio(client, BUCKET, f"{cidade}_forecast_df.json", forecast_df.to_dict(orient="records"))
+
+            # Processar dados atuais de qualidade do ar
+            iaqi_data = data['data']['iaqi']
+            current_data = {
+                'pollutant': [key.upper() for key in iaqi_data.keys()],
+                'value': [value['v'] for value in iaqi_data.values()]
+            }
+            current_df = pd.DataFrame(current_data)
+            salvar_dados_minio(client, BUCKET, f"{cidade}_current_df.json", current_df.to_dict(orient="records"))
+        else:
+            print("Erro na resposta da API AQICN:", data['data'])
+    else:
+        print(f"Erro ao obter dados da API AQICN: {response_aqicn.status_code}")
+        print(response_aqicn.text)
+
+# Executar funções de extração e processamento
 try:
-    raw_extract_data_api()
+    raw_extract_mapbiomas_api()
+    raw_extract_aqicn_api("vitoria")
 except Exception as e:
     print(e)
